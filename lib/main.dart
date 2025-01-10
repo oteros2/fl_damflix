4beb94cd3debba3050b9490cf58e6db5ab2d0c90
Jose Oteros <oteros19@gmail.com>
2025-01-10 09:05:18 +0100

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:fl_damflix/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DAMFLIX',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'details': (context) => const DetailsScreen(),
+      },
+    );
+  }
+}
